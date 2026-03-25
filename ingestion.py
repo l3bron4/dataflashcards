8741f92b4043f5485efec7b3ab376fd4b04dceac
l3bron4 <140657537+l3bron4@users.noreply.github.com>
2026-03-25 13:20:26 +0100

    Update ingestion.py
    add ingested_at column

--- ingestion.py
+++ ingestion.py
@@ -1,36 +1,43 @@
 import os
 import json
+import datetime
 from google.cloud import firestore
 from google.cloud import bigquery
 from google.oauth2 import service_account
 
 # 1. CONFIGURATION CROSS-PROJECT
-SOURCE_PROJECT_ID = "dataflashcards"       # Là où est ton Firestore
-DEST_PROJECT_ID = "my-project-florent-bq"  # Ton projet BigQuery habituel
+SOURCE_PROJECT_ID = "dataflashcards"       # Où est ton Firestore
+DEST_PROJECT_ID = "my-project-florent-bq"  # Où est ton BigQuery
 DATASET_ID = "raw_data_dataflashcards"
 TABLE_ID = "analytics_promo_raw"
 COLLECTION_NAME = "analytics_promo"
 
 def run_ingestion():
     # 2. AUTHENTIFICATION
+    # On récupère la clé JSON depuis les secrets GitHub
     info = json.loads(os.environ.get("GCP_SA_KEY"))
     creds = service_account.Credentials.from_service_account_info(info)
 
-    # Initialisation des clients sur les DEUX projets différents
-    # On lit dans SOURCE et on écrit dans DEST
+    # Initialisation des clients
     db = firestore.Client(credentials=creds, project=SOURCE_PROJECT_ID)
     bq_client = bigquery.Client(credentials=creds, project=DEST_PROJECT_ID)
 
-    print(f"--- Extraction de Firestore ({SOURCE_PROJECT_ID} / {COLLECTION_NAME}) ---")
+    print(f"--- Extraction de Firestore ({SOURCE_PROJECT_ID}) ---")
     
     docs = db.collection(COLLECTION_NAME).stream()
     rows_to_insert = []
+
+    # On récupère l'heure actuelle une seule fois pour tout le lot
+    now_timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat()
 
     for doc in docs:
         data = doc.to_dict()
         data["document_id"] = doc.id
         
-        # Nettoyage des dates pour BigQuery
+        # AJOUT DE LA COLONNE TECHNIQUE
+        data["ingested_at"] = now_timestamp
+        
+        # Nettoyage des formats de date pour BigQuery
         for key, value in data.items():
             if hasattr(value, 'isoformat'):
                 data[key] = value.isoformat()
@@ -39,10 +46,11 @@ def run_ingestion():
 
     print(f"Extraction réussie : {len(rows_to_insert)} documents.")
 
-    # 3. CHARGEMENT VERS LE BIGQUERY HABITUEL
+    # 3. CHARGEMENT VERS BIGQUERY
     if rows_to_insert:
         table_ref = f"{DEST_PROJECT_ID}.{DATASET_ID}.{TABLE_ID}"
         
+        # WRITE_TRUNCATE : vide la table et remplace par les nouvelles données
         job_config = bigquery.LoadJobConfig(
             write_disposition="WRITE_TRUNCATE",
             autodetect=True,
@@ -52,7 +60,7 @@ def run_ingestion():
         job = bq_client.load_table_from_json(
             rows_to_insert, table_ref, job_config=job_config
         )
-        job.result() 
+        job.result() # Attend la fin du job
 
         print(f"--- Succès ! Données disponibles dans {DEST_PROJECT_ID} ---")
     else:
